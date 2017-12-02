e3064984d5e9cbba8ed6a53414982dd4700290ea
Unknown <garnier.gaetan68@gmail.com>
2017-12-02 21:11:06 +0100

    Add rolejoueur

--- bot.js
+++ bot.js
@@ -6,9 +6,23 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!rolejoueur') {
+		
+let role = message.guild.roles.find("name", "JOUEUR");
+
+// Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
+let member = message.mentions.members.first();
+
+// or the person who made the command: let member = message.member;
+
+// Add the role!
+member.addRole(role).catch(console.error);
+
+// Remove a role!
+member.removeRole(role).catch(console.error);
   	}
+	
+	
 });
 
 // THIS  MUST  BE  THIS  WAY
